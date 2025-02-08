7d8fa02c44315da0b51933b0b681a233d67d42ed
Rodrigo Masid <remd884@mailfence.com>
2025-02-08 16:27:33 +0100

    17

--- src/_17HistogramaArreglo.java
+++ src/_17HistogramaArreglo.java
@@ -11,7 +11,34 @@ Para la tarea usaremos el asterisco(*) como representación gráfica para el his
 6: **
  */
 
+import java.util.Arrays;
+
 public class _17HistogramaArreglo {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    int arrLgth = 12;
+    int[] numArr = new int[arrLgth];
+
+    for (int i = 0; i < arrLgth; i++) {
+      numArr[i] = (int) ((Math.random() * 6) + 1);
+    }
+    System.out.println("El arreglo de números es " + Arrays.toString(numArr));
+
+    int[] counterArr = new int[6]; // Para contar del 1 al 6
+    for (int i = 0; i < arrLgth; i++) {
+      int num = numArr[i];
+      counterArr[num - 1]++;
+    }
+
+    for (int i = 0; i < 6; i++) {
+      System.out.print((i + 1) + ": "); // Muestra el número (1-6)
+
+      // Imprimir asteriscos
+      for (int j = 0; j < counterArr[i]; j++) {
+        System.out.print("*");
+      }
+
+      System.out.println(); // Salto de línea
+    }
+  }
 }
